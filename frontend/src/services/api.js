d06f88ecd142e301cf5e5743c2df811fb004435b
teena14 <teena14095@gmail.com>
2026-07-01 19:53:56 +0530

    deployment base api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
